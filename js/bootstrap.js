bebe10f62838b8737aa43970847b4532d4b5290b
Sebastian Abondano <sabondano1@gmail.com>
2014-07-09 10:06:40 -0400

    updated navigation_bar.html partial on _includes folder, removed existing navbar on xdrone nano page to test partial, added jquery to assign active class to navbar elements dynamically

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2363,7 +2363,28 @@ $('.thumbnail').on('mouseleave', function() {
 $('#callouts').children().popover();
  
 
+/*menu handler*/
+$(document).ready(function() {
+  $(function(){
+    function stripTrailingSlash(str) {
+      if(str.substr(-1) == '/') {
+        return str.substr(0, str.length - 1);
+      }
+      return str;
+    }
 
+    var url = window.location.pathname;  
+    var activePage = stripTrailingSlash(url);
+
+    $('.nav li a').each(function(){  
+      var currentPage = stripTrailingSlash($(this).attr('href'));
+
+      if (activePage == currentPage) {
+        $(this).parent().addClass('active'); 
+      } 
+    });
+  });
+});
 
 
 
